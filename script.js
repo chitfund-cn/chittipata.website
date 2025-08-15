fef6c4707985104cbc54d410b7e0f05024dc29b8
chitfunds <suramnaveen3526@gmail.com>
2025-08-15 18:30:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,16 +46,3 @@ async function verifyOTP() {
     alert("Invalid OTP");
   }
 }
-document.addEventListener("DOMContentLoaded", () => {
-  const upiApp = document.getElementById("upiApp");
-  const qrCode = document.getElementById("qrCode");
-
-  if (upiApp && qrCode) {
-    document.querySelectorAll('input[name="paymentMethod"]').forEach(el => {
-      el.addEventListener("change", () => {
-        upiApp.style.display = el.value === "upi" ? "block" : "none";
-        qrCode.style.display = el.value === "qr" ? "block" : "none";
-      });
-    });
-  }
-});
